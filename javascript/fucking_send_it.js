a9b66031533f426db26816381eb33cdee3877197
Lyssieth <lys@lys.ee>
2023-05-20 18:41:27 +0300

    Actually fix it?

--- javascript/fucking_send_it.js
+++ javascript/fucking_send_it.js
@@ -1,14 +1,34 @@
-window.stwp = {
+var stwp = {
     latestPrompts: {
         prompt: "",
-        negativePrompt: ""
+        negative: ""
     }
 };
 
+/**
+ * Checks whether the new prompt isn't equal to the currnet prompt, thus making it a check for whether the new prompt is *new* or just the same.
+ * @param {{prompt: string, negative: string}} prompts The new prompt
+ * @returns Whether the new prompt is different from the current prompt
+ */
+function stwp_is_new_prompt(prompts) {
+    let current = stwp.latestPrompts;
+
+    return prompts.prompt !== current.prompt || prompts.negative !== current.negative
+}
+
+/**
+ * A wrapper function for the async version :3
+ * @param {string} username The username to use on the webhook
+ * @param {string} url The URL of the webhook
+ */
 function stwp_send_to_webhook(username, url) {
     stwp_just_fucking_send_it(username, url).then(console.log("Sent!")).catch(console.warn);
 }
 
+/**
+ * Gathers the current prompt and negative prompt from the UI
+ * @returns {{prompt: string, negative: string}} The prompts, taken from the UI
+ */
 function stwp_get_prompts() {
     const gr = gradioApp();
 
@@ -19,10 +39,16 @@ function stwp_get_prompts() {
 
     return {
         prompt: prompt.value,
-        negativePrompt: negativePrompt.value
+        negative: negativePrompt.value
     }
 }
 
+/**
+ * Sends an image to a webhook URL using a specified username.
+ * @param {string} username - The username to use when sending the image.
+ * @param {string} url - The URL of the webhook to send the image to.
+ * @returns {Promise<void>} - A Promise that resolves when the image has been sent successfully.
+ */
 async function stwp_just_fucking_send_it(username, url) {
     const imageData = await stwp_steal_image_from_gallery();
     const imageFile = stwp_make_it_quack(imageData);
@@ -31,7 +57,7 @@ async function stwp_just_fucking_send_it(username, url) {
     const prompts = stwp_get_prompts();
     let resendPrompts = false;
 
-    if (window.stwp.latestPrompts !== prompts) {
+    if (stwp_is_new_prompt(prompts)) {
         window.stwp.latestPrompts = prompts;
         resendPrompts = true;
     }
@@ -41,7 +67,7 @@ async function stwp_just_fucking_send_it(username, url) {
     let content = "A new image just dropped!";
 
     if (resendPrompts) {
-        content += `\nPrompt:\`\`\`${prompts.prompt}\`\`\` Negative:\`\`\`${prompts.negativePrompt}\`\`\``;
+        content += `\nPrompt:\`\`\`${prompts.prompt}\`\`\` Negative:\`\`\`${prompts.negative}\`\`\``;
     }
 
     formData.append("payload_json", JSON.stringify({
@@ -61,6 +87,10 @@ async function stwp_just_fucking_send_it(username, url) {
     }
 }
 
+/**
+ * Steals an image from the gallery.
+ * @returns {Promise<string>} The data URL of the image
+ */
 async function stwp_steal_image_from_gallery() {
     var buttons = gradioApp().querySelectorAll(
         '[style="display: block;"].tabitem div[id$=_gallery] .thumbnail-item.thumbnail-small'
@@ -72,7 +102,7 @@ async function stwp_steal_image_from_gallery() {
     if (!button) button = buttons[0];
 
     if (!button)
-        throw new Error("[openoutpaint] No image available in the gallery");
+        throw new Error("[stwp] No image available in the gallery");
 
     const canvas = document.createElement("canvas");
     const image = document.createElement("img");
@@ -88,6 +118,11 @@ async function stwp_steal_image_from_gallery() {
     return canvas.toDataURL("image/png");
 }
 
+/**
+ * Converts a data URL to a file.
+ * @param {string} dataurl A data URL
+ * @returns {File} A file
+ */
 function stwp_make_it_quack(dataurl) {
     var arr = dataurl.split(","),
         mime = arr[0].match(/:(.*?);/)[1],
@@ -97,5 +132,5 @@ function stwp_make_it_quack(dataurl) {
     while (n--) {
         u8arr[n] = bstr.charCodeAt(n);
     }
-    return new File([u8arr], "openOutpaint-file", { type: mime });
+    return new File([u8arr], "stwp-file", { type: mime });
 }
